d930d3b6bdc3b8e012aecdd45ebf308c555c3ef9
Qasim Zaib <qasim.zaib@systemsltd.com>
2023-09-05 01:10:07 +0500

    Added custom tailwind fontsize classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<p className="text-blue-400">Hello World</p>
+			<p className="text-blue-400 text-7xl">Hello World</p>
 		</main>
 	);
 }
