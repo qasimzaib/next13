32927fce05a2a3ee5de64ee00b758ac6bb3db99e
Qasim Zaib <qasim.zaib@systemsltd.com>
2023-09-06 16:47:38 +0500

    Removed code

--- src/app/search/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import SearchBar from "@/app/components/SearchBar";
-
-export default function Header() {
-	return (
-		<div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-			<SearchBar />
-		</div>
-	);
-}
