b07749a12d714000b731a885765243aeb383c9d9
Qasim Zaib <qasim.zaib@systemsltd.com>
2023-09-07 11:07:08 +0500

    Removed code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,12 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
-import { Cuisine, Location, PRICING, PrismaClient } from "@prisma/client";
 
-export interface RestaurantCardType {
-	id: number;
-	name: string;
-	image: string;
-	slug: string;
-	cuisine: Cuisine;
-	location: Location;
-	pricing: PRICING;
-}
-
-const prisma = new PrismaClient();
-
-const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
-	const restaurants = await prisma.restaurant.findMany(
-		{select: {
-			id: true,
-			name: true,
-			image: true,
-			slug: true,
-			pricing: true,
-			location: true,
-			cuisine: true,
-		}}
-	);
-	return restaurants;
-}
-
-export default async function Home() {
-	const restaurants = await fetchRestaurants();
-
+export default function Home() {
 	return (
 		<main>
 			<Header />
 			<div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-				{restaurants.map((restaurant) => (
-					<RestaurantCard restaurant={restaurant} />
-				))}
+				<RestaurantCard />
 			</div>
 		</main>
 	);
